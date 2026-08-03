39dc66ba369c5b419cfdf3a9e8dc6d3f2e0fd8ea
Sujay <worker.station.10@gmail.com>
2026-08-04 01:43:10 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  trailingSlash: false,
 };
 
 export default nextConfig;
